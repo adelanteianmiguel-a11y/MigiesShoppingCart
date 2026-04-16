b3b8cec9d02cce74466dfc79a8e1f8daa41c6cd8
adelanteianmiguel-a11y <adelanteianmiguel@gmail.com>
2026-04-16 08:54:12 +0800

    products, cartslot, main program
    first commit

---
+++ Adelante_ShoppingCart/Adelante_ShoppingCart/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adelante_ShoppingCart
+{
+    internal class Product
+    {
+        // This class is for every drink chilling in the cooler
+        public int Id;
+        public string Name;
+        public double Price;
+        public int RemainingStock;
+
+        // When we create a new drink item, we fill in its info here
+        public Product(int id, string name, double price, int stock)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            RemainingStock = stock;
+        }
+
+        // Additional methods
+
+        // Quick total: price x quantity
+        public double GetItemTotal(int quantity)
+        {
+            return Price * quantity;
+        }
+
+        // Checks if there's enough stock left
+        public bool HasEnoughStock(int quantity)
+        {
+            return quantity <= RemainingStock;
+        }
+
+        // Updates stock after buying
+        public void DeductStock(int quantity)
+        {
+            RemainingStock -= quantity;
+        }
+
+        // Must have method
+
+        // To Show the drinks
+        public void DisplayProduct()
+        {
+            Console.WriteLine($"{Id}. {Name,-12} Price: {Price,8:F2} Stock: {RemainingStock}");
+        }
+    }
+}
